3e5aa5ce656429c55109188a39ad1f037b1c9616
Orenkfu <37892917+Orenkfu@users.noreply.github.com>
2018-06-12 21:17:34 +0300

    Update details.ts

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -2,12 +2,6 @@ import { GithubProvider } from './../../providers/github/github';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the DetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
